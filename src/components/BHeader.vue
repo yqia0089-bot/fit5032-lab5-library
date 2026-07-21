ab6c9ea2e877f25f36b7bcfeac0b75df014813ac
Yikai Qiao <265744607+yqia0089-bot@users.noreply.github.com>
2026-07-21 09:22:51 +0800

    feat: add Firebase user registration

--- src/components/BHeader.vue
+++ src/components/BHeader.vue
@@ -1,58 +1,43 @@
 <template>
   <div class="container">
-    <header class="d-flex justify-content-between align-items-center py-3">
-      <ul class="nav nav-pills">
+    <header class="py-3 border-bottom">
+      <ul class="nav nav-pills justify-content-center">
         <li class="nav-item">
-          <router-link to="/" class="nav-link" active-class="active">
+          <router-link
+            to="/"
+            class="nav-link"
+            exact-active-class="active"
+          >
             Home
           </router-link>
         </li>
 
-        <li v-if="authenticated" class="nav-item">
-          <router-link to="/about" class="nav-link" active-class="active">
+        <li class="nav-item">
+          <router-link
+            to="/about"
+            class="nav-link"
+            active-class="active"
+          >
             About
           </router-link>
         </li>
 
-        <li v-if="!authenticated" class="nav-item">
-          <router-link to="/login" class="nav-link" active-class="active">
-            Login
+        <li class="nav-item">
+          <router-link
+            to="/firebase-register"
+            class="nav-link"
+            active-class="active"
+          >
+            Firebase Register
           </router-link>
         </li>
       </ul>
-
-      <button
-        v-if="authenticated"
-        type="button"
-        class="btn btn-outline-danger"
-        @click="handleLogout"
-      >
-        Logout
-      </button>
     </header>
   </div>
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import { isAuthenticated, logout } from '../services/auth'
-
-const route = useRoute()
-const router = useRouter()
-
-const authenticated = ref(isAuthenticated())
-
-watch(
-  () => route.fullPath,
-  () => {
-    authenticated.value = isAuthenticated()
-  }
-)
-
-const handleLogout = () => {
-  logout()
-  authenticated.value = false
-  router.push({ name: 'Login' })
-}
+defineOptions({
+  name: 'BHeader',
+})
 </script>
