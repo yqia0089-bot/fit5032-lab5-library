872c2733ffdeafed0dbb90793f1b4f2bcbee6c77
Yikai Qiao <265744607+yqia0089-bot@users.noreply.github.com>
2026-07-28 13:00:07 +0800

    fix: prepare weather page for Cloudflare deployment

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+import {
+  fileURLToPath,
+  URL,
+} from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
+
   plugins: [
     vue(),
-    vueDevTools(),
   ],
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(
+        new URL('./src', import.meta.url),
+      ),
+    },
+  },
 })
